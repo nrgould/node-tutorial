10dc3256a4c3985c3f73fa6948d1f4cc10fa1a59
nrgould <nrgould197@gmail.com>
2024-10-24 13:29:44 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+	res.status(404).send('<h1>Page Not Found.</h1>');
+});
+
+app.listen(3000);
